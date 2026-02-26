37e56edd1df884e34ce796760370049af6f04572
beng888 <lawrenceardosa@gmail.com>
2026-02-26 15:04:01 +0800

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Optimizes the build for deployment
 };
 
 export default nextConfig;
